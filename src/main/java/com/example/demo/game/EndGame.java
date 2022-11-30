c6f5b629fdd86116b31dc1f09ec92a19cd57c890
Ozpin <78823905+ProfOzpin@users.noreply.github.com>
2022-11-30 21:37:24 +0400

    Add files via upload

--- src/main/java/com/example/demo/game/EndGame.java
+++ src/main/java/com/example/demo/game/EndGame.java
@@ -1,5 +1,4 @@
 package com.example.demo.game;
-
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,11 +10,18 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Optional;
+import java.util.Scanner;
 
 
 public class EndGame {
+
+
     private static EndGame singleInstance = null;
     private EndGame(){
 
@@ -32,12 +38,24 @@ public class EndGame {
         text.setFont(Font.font(80));
         root.getChildren().add(text);
 
-
-        Text scoreText = new Text(score+"");
+        Text scoreText = new Text("Score: "+score);
         scoreText.setFill(Color.BLACK);
-        scoreText.relocate(250,600);
+        scoreText.relocate(250,350);
         scoreText.setFont(Font.font(80));
         root.getChildren().add(scoreText);
+
+        Text highscore = new Text("");
+        highscore.setFill(Color.BLACK);
+        highscore.relocate(250,450);
+        highscore.setFont(Font.font(80));
+        root.getChildren().add(highscore);
+
+        Text highscore_alert = new Text("");
+        highscore_alert.setFill(Color.BLACK);
+        highscore_alert.relocate(250,550);
+        highscore_alert.setFont(Font.font(80));
+        root.getChildren().add(highscore_alert);
+
 
         Button quitButton = new Button("QUIT");
         quitButton.setPrefSize(100,30);
@@ -58,6 +76,38 @@ public class EndGame {
                 }
             }
         });
+
+        long stored_highscore = 0;
+        String line = "";
+        File obj = new File("src/main/java/com/example/demo/game/highscore.txt");
+        try {
+            Scanner scanner = new Scanner(obj);
+            line = scanner.nextLine();
+            stored_highscore = Long.parseLong(line);
+            scanner.close();
+            highscore.setText("Highscore: "+stored_highscore);
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if(score > stored_highscore){
+            try {
+
+                FileWriter writer = new FileWriter(obj, false);
+                writer.write(Long.toString(score));
+                writer.flush();
+                writer.close();
+                highscore_alert.setText("New Highscore!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
 
 
 
